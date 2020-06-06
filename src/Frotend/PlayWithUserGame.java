14eefc37023aa2f16cf8840e7fe5a85d70c3263c
“tankudo” <ankudo@icloud.com>
2020-06-06 10:02:28 +0200

    trying to JTable with ArrayList

--- src/Frotend/PlayWithUserGame.java
+++ src/Frotend/PlayWithUserGame.java
@@ -4,19 +4,39 @@ import backend.NotCorrectSizeWordException;
 import backend.PlayWithUser;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class PlayWithUserGame extends Menu implements LayoutManager, ActionListener {
 
     Image background;
     PlayWithUser playWithUser;
     JTextField giveMeAWord;
-    JFrame playWhithUserGame;
+    JFrame pUserG;
     JButton checkWord;
     WithWhoToPlay withWhoToPlay;
     JButton back;
+    //JTable
+    JTable table;
+    DefaultTableModel model = new DefaultTableModel();
+    JScrollPane scroll;
+    String headers[] = {"Your try", "result"};
+    String[] data = {"", ""};
+    ArrayList<String> ar;
+
+    public void insert() {
+        for (int i = 0; i < data.length; i++) {
+            ar.add(data[i]);
+        }
+
+        for (int i = 0; i < (ar.size() / 2); i++) {
+            model.addRow(new Object[]{String.valueOf(ar.get(2 * i)),
+                    String.valueOf(ar.get((2 * i) + 1))});
+        }
+    }
 
     public PlayWithUserGame() {
         this.background = Toolkit.getDefaultToolkit().createImage(".jpg");
@@ -24,6 +44,7 @@ public class PlayWithUserGame extends Menu implements LayoutManager, ActionListe
         this.checkWord = new JButton("Check your word");
         this.back = new JButton("Back");
         this.withWhoToPlay = new WithWhoToPlay();
+
 
     }
 
@@ -49,13 +70,13 @@ public class PlayWithUserGame extends Menu implements LayoutManager, ActionListe
 
 
     public void frameWindow() {
-        playWhithUserGame = new JFrame();
-        playWhithUserGame.setTitle("Play with user");
-        playWhithUserGame.setSize(450, 270);
-        playWhithUserGame.setVisible(true);
-        playWhithUserGame.setContentPane(this);
-        playWhithUserGame.setResizable(false);
-        playWhithUserGame.setLocationRelativeTo(null);
+        pUserG = new JFrame();
+        pUserG.setTitle("5 letters word");
+        pUserG.setSize(450, 270);
+        pUserG.setVisible(true);
+        pUserG.setContentPane(this);
+        pUserG.setResizable(false);
+        pUserG.setLocationRelativeTo(null);
         window();
     }
 
@@ -90,36 +111,46 @@ public class PlayWithUserGame extends Menu implements LayoutManager, ActionListe
         playWithUser = new PlayWithUser();
         if (e.getActionCommand().equals("Check your word")) {
             try {
-                //withWhoToPlay.getSecretWord()
-                String kutya = "kutya";
-                char [] kutyaC = kutya.toCharArray();
-                int check = playWithUser.checkTheWord(kutyaC, giveMeAWord.getText());
-                String[][] data = new String[2][2];
-                data[0][0] = giveMeAWord.getText();
-                data[0][1] = Integer.toString(check);
-                data[1][0] = null;
-                data[1][1] = null;
+                int check = playWithUser.checkTheWord(withWhoToPlay.getSecretWord(), giveMeAWord.getText());
+                data[0] = String.valueOf(check);
+                data[1] = giveMeAWord.getText();
+                //JTable
+                table = new JTable();
+                model.setColumnIdentifiers(headers);
+                table.setModel(model);
+                scroll = new JScrollPane(table);
+                ar = new ArrayList<>();
 
-                // Column Names
-                String[] columnNames = {"Your try", "result"};
+                add(scroll, BorderLayout.NORTH);
+                setSize(300, 300);
+                setVisible(true);
 
-                // Initializing the JTable
-                JFrame f = new JFrame();
-                JTable j = new JTable(data, columnNames);
-                j.setBounds(30, 40, 200, 300);
-                f.setSize(500, 200);
-                f.setLocationRelativeTo(null);
-
-                JScrollPane sp = new JScrollPane(j);
-                f.add(sp);
-                f.setVisible(true);
-
+//                String[][] data = new String[2][2];
+//                data[0][0] = giveMeAWord.getText();
+//                data[0][1] = Integer.toString(check);
+//                data[1][0] = null;
+//                data[1][1] = null;
+//
+//                // Column Names
+//                String[] columnNames = {"Your try", "result"};
+//
+//                // Initializing the JTable
+//                JFrame f = new JFrame();
+//                JTable j = new JTable(data, columnNames);
+//                j.setBounds(30, 40, 200, 300);
+//                f.setSize(500, 200);
+//                f.setLocationRelativeTo(null);
+//
+//                JScrollPane sp = new JScrollPane(j);
+//                f.add(sp);
+//                f.setVisible(true);
+                insert();
             } catch (NotCorrectSizeWordException ex) {
                 JOptionPane.showMessageDialog(null, ex.getMessage());
             }
         }
         if (e.getActionCommand().equals("Back")) {
-            playWhithUserGame.setVisible(false);
+            pUserG.setVisible(false);
         }
     }
 }
